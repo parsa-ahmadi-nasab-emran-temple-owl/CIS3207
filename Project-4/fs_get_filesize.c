63d09d249a7836abc4fcc8b913824ab53f404b04
Parsa Ahmadi Nasab Emran <parsaahmadinasabemran@Parsas-MacBook-Air.local>
2019-11-27 11:43:40 -0500

    updated version of fs_get_filesize.c

--- Project-4/fs_get_filesize.c
+++ Project-4/fs_get_filesize.c
@@ -1,5 +1,11 @@
 #include "my_file_system.h";
 
 int fs_get_filesize(int fildes){
-    return 0;
+    if(!descriptors[fildes].file_descriptor_in_use){
+        printf("error: Invalid file descriptor.\n");
+        return -1;
+    }
+    else{
+        return allocation[descriptors[fildes].file_descriptor_in_use].file_size;
+    }
 }
