c573631170a1553e3e15aabccf47d08af6d11a43
Parsa Ahmadi Nasab Emran <parsaahmadinasabemran@Parsas-MacBook-Air.local>
2019-11-27 11:35:12 -0500

    created and updated version of find_free_file_descriptor.c

--- Project-4/find_free_file_descriptor.c
+++ Project-4/find_free_file_descriptor.c
@@ -1,8 +1,17 @@
-//
-//  find_free_file_descriptor.c
-//  
-//
-//  Created by Parsa Ahmadi Nasab Emran on 11/27/19.
-//
+#include "my_file_system.h";
 
-#include <stdio.h>
+int find_free_file_descriptor(int index_of_file){
+    if (index_of_file != NULL && index_of_file >= 0 && index_of_file <= 255){
+        for(int i = 0; i < 64; i++) {
+            if(descriptors[i].file_descriptor_in_use == false) {
+                descriptors[i].file_descriptor_in_use = true;
+                descriptors[i].alocated = index_of_file;
+                descriptors[i].offset_for_seeking = 0;
+                return i;
+            }
+        }
+        
+        printf("error: no available file descriptor.\n");
+        return -1;
+    }
+}
