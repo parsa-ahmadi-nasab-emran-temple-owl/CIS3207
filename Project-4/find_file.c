091910258be796e5154f175b41ace1b6254b4c39
Parsa Ahmadi Nasab Emran <parsaahmadinasabemran@Parsas-MacBook-Air.local>
2019-11-27 11:32:08 -0500

    created and updated version of find_file.c

--- Project-4/find_file.c
+++ Project-4/find_file.c
@@ -1,8 +1,14 @@
-//
-//  find_file.c
-//  
-//
-//  Created by Parsa Ahmadi Nasab Emran on 11/27/19.
-//
+#include "my_file_system.h";
 
-#include <stdio.h>
+int find_file(char* name){
+    if (name != NULL) {
+        for(int i = 0; i < 256; i++) {
+            if(allocation[i].file_descriptor_in_use == 1 && strcmp(allocation[i].name, name) == 0){
+                return i;
+            }
+        }
+    }
+    else{
+        return -1;
+    }
+}
